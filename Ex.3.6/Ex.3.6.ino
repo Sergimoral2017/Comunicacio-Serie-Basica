f615d977c7343f0daef173bb0cd8d167bb375ebc
SergiMoral2017 <sergimoraltorres@gmail.com>
2018-02-07 20:07:35 +0100

    hi
    holiwis

---
+++ Ex.3.6/Ex.3.6.ino
@@ -0,0 +1,79 @@
+/**********************************************************************************
+**                                                                               **
+**                          Control Structures                                   **
+**                                                                               **
+**                                                                               **
+**********************************************************************************/
+
+//********** Includes *************************************************************
+
+//********** Variables ************************************************************
+int hora=13;
+//********** Setup ****************************************************************
+void setup()
+{
+  Serial.begin(9600);
+
+   switch (hora)
+   {
+    case 0:
+    Serial.println("Temps Lliure");
+    case 1:
+    Serial.println("Temps Lliure");
+    case 2:
+    Serial.println("Temps Lliure");
+    case 3:
+    Serial.println("Temps Lliure");
+    case 4:
+    Serial.println("Temps Lliure");
+    case 5:
+    Serial.println("Temps Lliure");
+    case 6:
+    Serial.println("Temps Lliure");
+    case 7:
+    Serial.println("Temps Lliure");
+    case 8:
+    Serial.println("Temps Lliure");
+    case 9:
+    Serial.println("Treball");
+    case 10:
+    Serial.println("Treball");
+    case 11:
+    Serial.println("Treball");
+    case 12:
+    Serial.println("Treball");
+    case 12:
+    Serial.println("Treball");
+    case 13:
+    Serial.println("Treball");
+    case 14:
+    Serial.println("Treball");
+    case 15:
+    Serial.println("Treball");
+    case 16:
+    Serial.println("Treball");
+    case 17:
+    Serial.println("Temps Lliure");
+    case 18:
+    Serial.println("Temps Lliure");
+    case 19:
+    Serial.println("Temps Lliure");
+    case 20:
+    Serial.println("Temps Lliure");
+    case 21:
+    Serial.println("Temps Lliure");
+    case 22:
+    Serial.println("Temps Lliure");
+    case 23:
+    Serial.println("Temps Lliure");
+  
+  }
+  
+}
+
+//********** Loop *****************************************************************
+void loop()  
+{
+}
+
+//********** Funcions *************************************************************
